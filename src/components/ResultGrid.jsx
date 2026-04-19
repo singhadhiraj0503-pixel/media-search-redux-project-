dfca2b1ac10450c7f09638d15b14811f5da48413
singhadhiraj0503-pixel <singhadhiraj0503@gmail.com>
2026-04-20 02:25:40 +0530

    still-pending-2

--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { fetchPhotos, fetchVideos } from "../api/mediaApi";
 import {
   setQuery,
@@ -7,27 +7,65 @@ import {
   setResult,
 } from "../redux/features/searchSlice";
 import { useDispatch, useSelector } from "react-redux";
+import ResultCard from "./ResultCard";
 
 const ResultGrid = () => {
+  const dispatch = useDispatch();
   const { query, activeTab, result, loading, error } = useSelector(
     (store) => store.search,
   );
 
   const getData = async () => {
-    if (activeTab == "photos") {
-      let data = await fetchPhotos(query);
+    try {
+      let data;
+      if (activeTab == "photos") {
+        let response = await fetchPhotos(query);
+        data = response.results.map((item) => ({
+          id: item.id,
+          type: "photos",
+          title: item.alt_description,
+          thumbnail: item.urls.small,
+          src: item.urls.full,
+        }));
+      }
+      if (activeTab == "videos") {
+        let response = await fetchVideos(query);
+        data = response.data.videos.map((item) => ({
+          id: item.id,
+          type: "videos",
+          title: item.user.name || "video",
+          thumbnail: item.image,
+          src: item.video_files[0].link,
+        }));
+      }
+
+      dispatch(setResult(data));
       console.log(data);
+    } catch (err) {
+      dispatch(setError(err.message));
     }
   };
 
+  useEffect(
+    function () {
+      if (!query) return;
+      getData();
+    },
+    [query, activeTab],
+  );
+
+  if (error) return <h1>Error</h1>;
+  if (loading) return <h1>Loading...</h1>;
+
   return (
     <div className="w-full p-4">
-      <button
-        className="p-4 bg-blue-600 text-white rounded-2xl active:scale-95"
-        onClick={getData}
-      >
-        Get Data
-      </button>
+      {result.map((item, idx) => {
+        return (
+          <div key={idx}>
+            <ResultCard item={item} />
+          </div>
+        );
+      })}
     </div>
   );
 };
